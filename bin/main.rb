b0b0c375c1c6beae2103cbbefdf27ffe7e9982f6
andresporras3423 <andresporras3423@gmail.com>
2020-02-24 17:43:08 -0500

    modified flow of the game

--- bin/main.rb
+++ bin/main.rb
@@ -5,11 +5,18 @@ puts 'enter name of player 1:'
 player1 = gets.chomp
 puts 'enter name of player 2:'
 player2 = gets.chomp
+i=0
+puts 'show tic tac toe board'
 loop do
-  puts 'show tic tac toe board'
-  puts "#{player1} choose move"
+  puts "#{(i%2==0 ? player1 : player2)} choose move: "
   move = gets.chomp
   puts 'updated board'
-  puts "#{player2} choose move"
-  move = gets.chomp
+  i+=1
+  if i>=5 # is impossible to have winner before the fifth movement
+    puts "run method to see if current player has won the game"
+  end
+  if i==9 # on this point the board has not any movement available
+    puts "board is full, game can't continue"
+    break
+  end
 end
