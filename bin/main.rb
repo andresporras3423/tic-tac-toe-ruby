5a3a2d5efd094446b3021f13204c35d634b2d452
andresporras3423 <andresporras3423@gmail.com>
2020-02-24 17:29:42 -0500

    show basic flow of the program

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,15 @@
 #!/usr/bin/env ruby
 puts 'Hello World!'
+
+puts 'enter name of player 1:'
+player1 = gets.chomp
+puts 'enter name of player 2:'
+player2 = gets.chomp
+loop do
+  puts 'show tic tac toe board'
+  puts "#{player1} choose move"
+  move = gets.chomp
+  puts 'updated board'
+  puts "#{player2} choose move"
+  move = gets.chomp
+end
