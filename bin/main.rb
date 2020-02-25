77add6d977d16bd38f52abffdc8a8d528a7c31eb
andresporras3423 <andresporras3423@gmail.com>
2020-02-25 11:17:51 -0500

    update match class and main file

--- bin/main.rb
+++ bin/main.rb
@@ -1,5 +1,3 @@
-#!/usr/bin/env ruby
-
 require './player'
 require './match'
 
@@ -14,7 +12,31 @@ class Game
   end
 
   def begin_game
-    @match = Math.new([Player.new(player1, 'x'), Player.new(player2, '0')])
+    @match = Match.new([Player.new(player1, 'x'), Player.new(player2, '0')])
     play_game
   end
+
+  def play_game
+    play_turn while match.game_continue && match.turn < 9
+    if match.turn == 9 && match.game_continue
+      match.show_board
+      puts 'Game was a draw'
+    end
+  end
+
+  def play_turn
+    match.show_board
+    puts match.players[match.turn % 2].name + ', choose your movement (use available numbers): '
+    movement = gets.chomp.to_i
+    if match.movements[movement - 1] == '0' || match.movements[movement - 1] == 'x'
+      puts 'not available spot'
+    else
+      match.movements[movement - 1] = match.players[match.turn % 2].symbol
+      match.update_board
+      match.find_winner(movement)
+      match.turn += 1
+    end
+  end
 end
+
+Game.new
--- bin/match.rb
+++ bin/match.rb
@@ -1,19 +1,15 @@
 class Match
   attr_accessor :players, :turn, :board, :game_continue, :movements
-  def initialize
-    @players = []
-    default_values
-    begin_game
-  end
-
-  def default_values
+  def initialize(players)
+    @players = players
     @game_continue = true
     @turn = 0
     @board = [[' 1 ', '|', ' 2 ', '|', ' 3 '], ['---', '|', '---', '|', '---'], [' 4 ', '|', ' 5 ', '|', ' 6 '], ['---', '|', '---', '|', '---'], [' 7 ', '|', ' 8 ', '|', ' 9 ']]
     @movements = %w[1 2 3 4 5 6 7 8 9]
   end
 
-  def show_table
+
+  def show_board
     i = 0
     while i < board.length
       j = 0
@@ -23,28 +19,6 @@ class Match
       end
       print "\n"
       i += 1
-    end
-  end
-
-  def play_game
-    play_turn while game_continue && turn < 9
-    if turn == 9 && game_continue
-      show_table
-      puts 'Game was a draw'
-    end
-  end
-
-  def play_turn
-    show_table
-    puts players[turn % 2].name + ', choose your movement (use available spots numbers): '
-    movement = gets.chomp.to_i
-    if movements[movement - 1] == '0' || movements[movement - 1] == 'x'
-      puts 'not available spot'
-    else
-      movements[movement - 1] = players[turn % 2].symbol
-      update_board
-      find_winner(movement)
-      @turn += 1
     end
   end
 
@@ -65,7 +39,7 @@ class Match
     n2 = (move - 1) % 3
     line = players[turn % 2].symbol + players[turn % 2].symbol + players[turn % 2].symbol
     if (movements[n1 - 1] + movements[n1 - 2] + movements[n1 - 3] == line) || (movements[n2] + movements[n2 + 3] + movements[n2 + 6] == line) || (movements[0] + movements[4] + movements[8] == line) || (movements[2] + movements[4] + movements[6] == line)
-      show_table
+      show_board
       puts players[turn % 2].name + ' is the winner'
       @game_continue = false
     end
