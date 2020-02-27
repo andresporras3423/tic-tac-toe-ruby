01b0057f48d26a18d30a8026f6c915ff4650433f
andresporras3423 <andresporras3423@gmail.com>
2020-02-27 11:21:24 -0500

    added second test to the project

--- spec/match_spec.rb
+++ spec/match_spec.rb
@@ -75,6 +75,71 @@ RSpec.describe Match do
     end.to output(" x | 0 | x \n---|---|---\n 0 | x | 0 \n---|---|---\n x | 8 | 9 \nOscar is the winner\n").to_stdout
     expect(match.game_continue).to eql(false)
   end
+
+
+  it 'Test game 2, Ángel wins in the sixth movement' do
+    match = Match.new([Player.new('Oscar', 'x'), Player.new('Ángel', '0')])
+    expect do
+      match.show_board
+    end.to output(" 1 | 2 | 3 \n---|---|---\n 4 | 5 | 6 \n---|---|---\n 7 | 8 | 9 \n").to_stdout
+    expect(match.game_continue).to eql(true)
+    match.movements[1] = 'x'
+    match.update_board
+    expect do
+      match.find_winner(2)
+    end.to output("").to_stdout
+    expect(match.game_continue).to eql(true)
+    match.turn += 1
+    expect do
+      match.show_board
+    end.to output(" 1 | x | 3 \n---|---|---\n 4 | 5 | 6 \n---|---|---\n 7 | 8 | 9 \n").to_stdout
+    match.movements[0] = '0'
+    match.update_board
+    expect do
+      match.find_winner(1)
+    end.to output("").to_stdout
+    expect(match.game_continue).to eql(true)
+    match.turn += 1
+    expect do
+      match.show_board
+    end.to output(" 0 | x | 3 \n---|---|---\n 4 | 5 | 6 \n---|---|---\n 7 | 8 | 9 \n").to_stdout
+    match.movements[4] = 'x'
+    match.update_board
+    expect do
+      match.find_winner(5)
+    end.to output("").to_stdout
+    expect(match.game_continue).to eql(true)
+    match.turn += 1
+    expect do
+      match.show_board
+    end.to output(" 0 | x | 3 \n---|---|---\n 4 | x | 6 \n---|---|---\n 7 | 8 | 9 \n").to_stdout
+    match.movements[3] = '0'
+    match.update_board
+    expect do
+      match.find_winner(4)
+    end.to output("").to_stdout
+    expect(match.game_continue).to eql(true)
+    match.turn += 1
+    expect do
+      match.show_board
+    end.to output(" 0 | x | 3 \n---|---|---\n 0 | x | 6 \n---|---|---\n 7 | 8 | 9 \n").to_stdout
+    match.movements[8] = 'x'
+    match.update_board
+    expect do
+      match.find_winner(9)
+    end.to output("").to_stdout
+    expect(match.game_continue).to eql(true)
+    match.turn += 1
+    expect do
+      match.show_board
+    end.to output(" 0 | x | 3 \n---|---|---\n 0 | x | 6 \n---|---|---\n 7 | 8 | x \n").to_stdout
+    match.movements[6] = '0'
+    match.update_board
+    expect do
+      match.find_winner(7)
+    end.to output(" 0 | x | 3 \n---|---|---\n 0 | x | 6 \n---|---|---\n 0 | 8 | x \nÁngel is the winner\n").to_stdout
+    expect(match.game_continue).to eql(false)
+  end
 end
 
 # def hello_world()
